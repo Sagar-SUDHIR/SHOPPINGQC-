7e1a76a417b1c17ca322d6d9c6d204923508b427
Sagar-SUDHIR <zero2startupsagar@gmail.com>
2025-07-05 16:13:57 +0530

    Uploaded all files

---
+++ product.js
@@ -0,0 +1,83 @@
+const products = [
+  {
+    id: 1,
+    name: "Red Shoe",
+    price: "$7.5",
+    image: "sh.jpg",
+    category: "Footwear",
+    description: "Sporty look plain red shoe",
+    sizes: [7, 8, 9, 10, 11]
+  },
+  {
+    id: 2,
+    name: "Metal Watch",
+    price: "$10",
+    image: "wt.jpg",
+    category: "Accessories",
+    description: "Premium, glossy look metal watch",
+    sizes: []
+  },
+  {
+    id: 3,
+    name: "Red T-shirt",
+    price: "$5",
+    image: "gb.jpg",
+    category: "Clothing",
+    description: "Cotton plain red T-shirt",
+    sizes: ["S", "M", "L", "XL"]
+  },
+  {
+    id: 4,
+    name: "Beige & White Shoe",
+    price: "$7.5",
+    image: "i1.jpg",
+    category: "Footwear",
+    description: "Sporty look beige & white shoe",
+    sizes: [7, 8, 9, 10]
+  },
+  {
+    id: 5,
+    name: "Smart Watch",
+    price: "$10",
+    image: "i2.jpg",
+    category: "Electronics",
+    description: "Premium, glossy look smart watch",
+    sizes: []
+  },
+  {
+    id: 6,
+    name: "Green Metal Watch",
+    price: "$5",
+    image: "i3.jpg",
+    category: "Accessories",
+    description: "Glossy green metal watch",
+    sizes: []
+  },
+  {
+    id: 7,
+    name: "Sunglasses",
+    price: "$7.5",
+    image: "i4.jpg",
+    category: "Fashion",
+    description: "Glossy stylish sunglasses",
+    sizes: []
+  },
+  {
+    id: 8,
+    name: "Ladies Dress",
+    price: "$10",
+    image: "i5.jpg",
+    category: "Clothing",
+    description: "Premium, glossy look ladies dress",
+    sizes: ["S", "M", "L"]
+  },
+  {
+    id: 9,
+    name: "Patterned Men's Shirt",
+    price: "$5",
+    image: "i6.jpg",
+    category: "Clothing",
+    description: "Cotton patterned cheetah-style shirt",
+    sizes: ["M", "L", "XL"]
+  }
+];
